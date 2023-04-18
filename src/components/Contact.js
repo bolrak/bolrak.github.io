3083af92ec88511e1cd758e85fb71c91301c6b0f
Pablo Inzunza <56041130+bolrak@users.noreply.github.com>
2023-04-18 16:42:17 -0400

    Avances contact form

---
+++ src/components/Contact.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+
+export const Contact = () => {
+  const formInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  const [formDetails, setformDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+
+const onFormUpdate = (category, value)=>{
+    setformDetails({
+        ...formDetails,
+        [category]:value
+    })
+}
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Contactame" />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form>
+                <Row>
+                    <Col sm={6} className="px-1">
+                        <input type="text" value={formDetails.firstName} placeholder="Primer Nobre" onChange= {(e) => onFormUpdate('firstName', e.target.value)}/>
+                    </Col>
+                    
+                    <Col sm={6} className="px-1">
+                        <input type="text" value={formDetails.lastName} placeholder="Apellidos" onChange= {(e) => onFormUpdate('lastName', e.target.value)}/>
+                    </Col>
+                    <Col sm={6} className="px-1">
+                        <input type="text" value={formDetails.email} placeholder="Correo Electrónico" onChange= {(e) => onFormUpdate('email', e.target.value)}/>
+                    </Col>
+                    
+                    <Col sm={6} className="px-1">
+                        <input type="text" value={formDetails.phone} placeholder="Número Telefónico" onChange= {(e) => onFormUpdate('phone', e.target.value)}/>
+                    </Col>
+
+                </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
