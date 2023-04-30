e68c150b3e745de16b1254c4bb5a73e79df4f88f
Pablo Inzunza <56041130+bolrak@users.noreply.github.com>
2023-04-30 14:06:48 -0400

    correcciones modal, aun faltan detalles

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,21 +1,30 @@
 import { Col } from "react-bootstrap";
 import Modal from "react-modal";
-import {useState} from 'react';
+import { useState } from 'react';
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Col sm={6} md={4}>
-      <div className="proj-imgbx" onClick={() => setIsModalOpen(true)}>
+      <div className="proj-imgbx" onClick={handleOpenModal}>
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4> {title}</h4>
           <span> {description}</span>
         </div>
-        <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}>
+        <Modal  scrollable={true} isOpen={isModalOpen} onRequestClose={handleCloseModal} className="my-modal" overlayClassName="my-modal-overlay" onClick={handleCloseModal}>
           <img src={imgUrl} />
-          <button onClick={() => setIsModalOpen(false)}>Cerrar</button>
         </Modal>
+
       </div>
     </Col>
   );
